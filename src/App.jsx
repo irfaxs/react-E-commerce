49f5bdeb3ffc659d54e6b4ae0ff1bee1b2c82ec5
irfaxs <irfanippu291@gmail.com>
2025-11-27 17:07:23 +0530

    irfan

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>irfan</h1>
+      <h1>irfan</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
